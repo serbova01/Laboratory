0a6f2c609768ce3d556b3110f42d1af1f11335b7
Nishinoya2001@outlook.com <Nishinoya2001@outlook.com>
2021-10-28 11:55:50 +0300

    Окончательный вариант программы

--- LB4/Program.cs
+++ LB4/Program.cs
@@ -5,27 +5,68 @@ namespace LB4
 {
     class Program
     {
+        string[] alf = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите число: ");
+            Console.Write("Введите число: ");
+            Program p = new Program();
             int s_num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите систему счисления: ");
+            Console.Write("Введите систему счисления: ");
             int s = Convert.ToInt32(Console.ReadLine());
-            int f_num = 0;
+            while (s > 36)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Система счисления не должна превышать 36!");
+                Console.WriteLine();
+                Console.Write("Введите систему счисления: ");
+                s = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine($"Результат: {p.WorkProgram(s_num, s)}");
+            List<int> list_i = new List<int>();
+            for (int i = 2; i < 37; i++)
+            {
+                string str = p.WorkProgram(23, i);
+                if (str[str.Length-1] == '2')
+                {
+                    list_i.Add(i);
+                }
+            }
+            string exp_str = "";
+            for (int i = 0; i < list_i.Count; i++)
+            {
+                if (i+1<list_i.Count)
+                {
+                    exp_str += $"{list_i[i]}, ";
+                }
+                else
+                    exp_str += list_i[i];
+            }
+            Console.Write("Cистемы счисления, в которых запись числа 23 оканчивается на 2: " + exp_str);
+            Console.ReadKey();
+        }
+        public string WorkProgram(int s_num, int s)
+        {
+            string f_num = "";
             Stack<int> stack = new Stack<int>();
-            while (s_num>0)
+            while (s_num > 0)
             {
                 int x = s_num % s;
                 stack.Push(x);
                 s_num /= s;
             }
-            int l = stack.Count;
-            for (int i = l-1; i >= 0 && stack.Count>0; i++)
+            while (stack.Count > 0)
             {
-                f_num += (int)(stack.Pop() * Math.Pow(10, i));
+                string x = "";
+                if (stack.Peek() >= 10)
+                {
+                    int o = stack.Pop() % 10;
+                    x = alf[o];
+                }
+                else
+                    x = $"{stack.Pop()}";
+                f_num += x;
             }
-            Console.WriteLine($"Результат: {f_num}");
-            Console.ReadKey();
+            return f_num;
         }
     }
 }
